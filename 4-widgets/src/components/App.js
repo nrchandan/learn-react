d032b1c8dcf8dc3e24ccfe1ebda0255054be2dc0
Chandan <chandan.gupta@gmail.com>
2021-01-17 09:02:55 +0530

    Add navigation to the widgets

--- 4-widgets/src/components/App.js
+++ 4-widgets/src/components/App.js
@@ -3,32 +3,43 @@ import Accordion from './Accordion';
 import Search from './Search';
 import Dropdown from './Dropdown';
 import Translate from './Translate';
+import Route from './Route';
+import Header from './Header';
 
 const App = () => {
 
   const [selected, setSelected] = useState(colorOptions[0]);
 
-  // const items = [
-  //   {
-  //     title: 'What is React?',
-  //     description: 'React is THE most popular web framework'
-  //   },
-  //   {
-  //     title: 'Why React?',
-  //     description: "Lets us create complex web applications"
-  //   }
-  // ]
+  const items = [
+    {
+      title: 'What is React?',
+      description: 'React is THE most popular web framework'
+    },
+    {
+      title: 'Why React?',
+      description: "Lets us create complex web applications"
+    }
+  ]
   return (
     <div>
-      {/* <Accordion items={items}/> */}
-      {/* <Search></Search> */}
-      {/* <Dropdown
-        label="Select a color"
-        items={colorOptions}
-        selected={selected}
-        setSelected={setSelected}>
-      </Dropdown> */}
-      <Translate></Translate>
+      <Header />
+      <Route path="/">
+        <Accordion items={items}/>
+      </Route>
+      <Route path="/list">
+        <Search></Search>
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          items={colorOptions}
+          selected={selected}
+          setSelected={setSelected}>
+        </Dropdown>        
+      </Route>
+      <Route path="/translate">
+        <Translate></Translate>
+      </Route>
     </div>
   );
 }
